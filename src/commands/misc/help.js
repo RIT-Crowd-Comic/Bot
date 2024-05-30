06b1f5825680bcd442da2ab05ef4a13432c96f33
nca6109 <nca6109@g.rit.edu>
2024-05-30 14:07:19 -0500

    Fixed typos and linted files

--- src/commands/misc/help.js
+++ src/commands/misc/help.js
@@ -1,4 +1,4 @@
-const { EmbedBuilder } = require('@discordjs/builders')
+const { EmbedBuilder } = require('@discordjs/builders');
 
 module.exports = {
     name: 'help',
@@ -9,22 +9,22 @@ module.exports = {
         const embed = new EmbedBuilder()
             .setTitle('Server Commands')
             .addFields(
-            {
+                {
                 // name - /[name of command]
-                name: '/help-remember',
-                // value - description of command
-                value: 'Brings up list of remember commands'
-            },
-            {
-                name: '/check-in-interface',
-                value: 'Create an interface for users to schedule their check ins'
-            },
-            {
-                name: '/schedule-check-in',
-                value: 'Schedule a day and time to be notified'
-            }
-        );
+                    name: '/help-remember',
+                    // value - description of command
+                    value: 'Brings up list of remember commands'
+                },
+                {
+                    name: '/check-in-interface',
+                    value: 'Create an interface for users to schedule their check ins'
+                },
+                {
+                    name: '/schedule-check-in',
+                    value: 'Schedule a day and time to be notified'
+                }
+            );
 
-        interation.reply({embeds: [embed], ephemeral: true});
+        interaction.reply({embeds: [embed], ephemeral: true});
     }
-}
+};
--- src/commands/misc/helpRemember.js
+++ src/commands/misc/helpRemember.js
@@ -1,47 +1,47 @@
-const { EmbedBuilder } = require('@discordjs/builders')
+const { EmbedBuilder } = require('@discordjs/builders');
 
 module.exports = {
     name: 'help-remember',
     description: 'List of commands for remembering messages',
 
-    callback: (client, interation) => {
+    callback: (client, interaction) => {
         //Create an embed to send to the user
         const embed = new EmbedBuilder()
-        .setTitle('Remember Commands')
-        .setDescription('Commands relating to message storage and retrieval')
-        .addFields(
-            {
+            .setTitle('Remember Commands')
+            .setDescription('Commands relating to message storage and retrieval')
+            .addFields(
+                {
                 // name - /[name of command]
-                name: '/remember',
-                // value - description of command
-                value: 'Stores a message from the current channel based on its message-id'
-            },
-            {
-                name: '/clear-messages',
-                value: 'Clear all messages currently saved in remembrance'
-            },
-            {
-                name: '/print-storage',
-                value: 'Recall and print stored messages'
-            },
-            {
-                name: '/recall',
-                value: 'creates a JSON of all the saved message'
-            },
-            {
-                name: '/remember-chunk-time',
-                value: 'Saves messages from past set amount of \"minutes\" in the current channel.'
-            },
-            {
-                name: '/remember-count',
-                value: 'Saves the last x messages from the current channel. Has an option to save from a specific channel.'
-            },
-            {
-                name: '/remember-messages',
-                value: 'Remember all messages between two specific messages (inclusivley)'
-            }
-        );
+                    name: '/remember',
+                    // value - description of command
+                    value: 'Stores a message from the current channel based on its message-id'
+                },
+                {
+                    name: '/clear-messages',
+                    value: 'Clear all messages currently saved in remembrance'
+                },
+                {
+                    name: '/print-storage',
+                    value: 'Recall and print stored messages'
+                },
+                {
+                    name: '/recall',
+                    value: 'creates a JSON of all the saved message'
+                },
+                {
+                    name: '/remember-chunk-time',
+                    value: 'Saves messages from past set amount of "minutes" in the current channel.'
+                },
+                {
+                    name: '/remember-count',
+                    value: 'Saves the last x messages from the current channel. Has an option to save from a specific channel.'
+                },
+                {
+                    name: '/remember-messages',
+                    value: 'Remember all messages between two specific messages (inclusivley)'
+                }
+            );
 
-        interation.reply({embeds: [embed], ephemeral: true});
+        interaction.reply({embeds: [embed], ephemeral: true});
     }
-}
+};
