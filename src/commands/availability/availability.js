7ad518d038fb38eb6e0622f6d0ec0e36152a397d
nca6109 <nca6109@g.rit.edu>
2024-06-03 15:36:08 -0500

    Began set-availability work

--- src/commands/availability/availability.js
+++ src/commands/availability/availability.js
@@ -9,24 +9,25 @@ const dayjs = require('dayjs');
 const { saveUnavailability } = require('../../utils/availability');
 const { start } = require('repl');
 
-// const availability = {
-//     userid: {
-//         id: '',
-//         tag: '',
-//         available: {
-//             ...scheduleItems,
-//             display: () => 'readable schedule'
-//         }, // schedule object
-//         unavailable: [
-//             // example
-//             {
-//                 from: Dayjs, // day and time
-//                 to: Dayjs, // day and time
-//                 display: () => 'readable dates'
-//             }
-//         ]
+//Availability Data Schema
+// [
+//     {
+//       "userID": "123",
+//       "userTag": "member",
+//       "available": {
+//         "from": "DayJS object",
+//         "to": "DayJS object",
+//         "days": "Monday-Friday"
+//       },
+//       "unavailable": [
+//         {
+//           "from": "DayJS object",
+//           "to": "DayJS object",
+//           "reason": "sick"
+//         }
+//       ]
 //     }
-// }
+// ]
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -60,6 +61,24 @@ module.exports = {
                         .setDescription('Why will you be unavailable?')
                         .setRequired(false)
                 )
+        )
+        .addSubcommand(subcommand => 
+            subcommand.setName('set-availability')
+            .setDescription('Provide the times of day and days of the week you are available')
+            .addStringOption(option =>
+                option.setName('time-from')
+                .setDescription('When you start working (Formats: 2:00 pm, 14:00)')
+                .setRequired(true)
+            )
+            .addStringOption(option => 
+                option.setName('time-to')
+                .setDescription('When you stop working (Formats: 2:00 pm, 14:00)')
+                .setRequired(true)
+            )
+            .addStringOption(option =>
+                option.setName('days')
+                .setDescription('Provide days you are available (Formats: Monday Tuesday..., M T W..., Monday-Friday')
+            )
         ),
 
     options:
@@ -73,7 +92,8 @@ module.exports = {
 
 
         const action = {
-            'set-unavailability' : () => setUnavail(interaction)
+            'set-unavailability' : () => setUnavail(interaction),
+            'set-availability' : () => setAvail(interaction),
         };
 
         try {
@@ -90,142 +110,6 @@ module.exports = {
         }
     }
 };
-
-//Pre-Refactor Code
-// module.exports = {
-//     deleted: false, //deleted (optional) specifies if this command shouldn't be on the server/guild
-//     name: 'remember-unavailable',  //a name(required)
-//     description: 'Record a time you\'ll be unavailable', //a description(required)
-//     devOnly: false, //a devonly flag(optional)
-//     testOnly: false, //a testonly flag(optional)
-//     //options(optional)
-//     options: [
-//         //Repeated days will not be scheduled as of now
-//         /*{
-//             name: 'schedule-days', //name(required)
-//             description: 'The name of days can be abbreviated as "m t w (th or h) f sa su". Ex: "Monday w f" or "daily"', //description(required)
-//             required: false, //required(optional) : makes it so the user needs to input something to run the command
-//             type: ApplicationCommandOptionType.String, //type of command, use intellisense or docs to select proper one
-//             //https://discord.com/developers/docs/interactions/application-commands
-
-//         },*/
-//         {
-//             name: 'date-from',
-//             description: 'List a specific date you will be unavailable (Formats: m/dd, m-dd, m/dd/yyyy...)',
-//             required: true,
-//             type: ApplicationCommandOptionType.String,
-//         },
-//         {
-//             name: 'date-to',
-//             description: 'List a specific date you will no longer be unavailable (Formats: m/dd, m-dd, m/dd/yyyy...)',
-//             required: true,
-//             type: ApplicationCommandOptionType.String,
-//         },
-//         {
-//             name: 'time-from',
-//             description: 'At what time of day will you be available? (Format Exs: 2:00 pm, 14:00)',
-//             required: false,
-//             type: ApplicationCommandOptionType.String,
-//         },
-//         {
-//             name: 'time-to',
-//             description: 'At what time of day are you no longer available? (Format Exs: 2:00 pm, 14:00)',
-//             required: false,
-//             type: ApplicationCommandOptionType.String,
-//         },
-//         {
-//             name: 'reason',
-//             description: 'Why will you be unavailable?',
-//             required: false,
-//             type: ApplicationCommandOptionType.String
-//         }
-//     ],
-//     permissionsRequired: [
-//         PermissionFlagsBits.ViewChannel,
-//     ], //permissions(optional) check intellisense or docs to view permission options
-//     //https://discord.com/developers/docs/topics/permissions
-
-//     /**
-//      *  * Parse and save schedule date to the database
-//      * @param {Client} client 
-//      * @param {CommandInteraction} interaction 
-//      */
-//     callback: async (client, interaction) => {
-
-//         const userId = interaction?.user?.id;
-//         const userTag = interaction?.user?.tag;
-
-//         await interaction.deferReply({ ephemeral: true });
-
-//         // command should include a user
-//         if (userId === undefined || userTag === undefined) {
-//             await interaction.editReply({
-//                 ephemeral: true,
-//                 content: 'Could not process command'
-//             });
-//             return;
-//         }
-
-//         try {
-//             //const scheduleDays = interaction.options.get('schedule-days')?.value;
-//             const dateFrom = interaction.options.get('date-from')?.value;
-//             const dateTo = interaction.options.get('date-to')?.value;
-//             let timeFrom = interaction.options.get('time-from')?.value;
-//             let timeTo = interaction.options.get('time-to')?.value;
-//             const reason = interaction.options.get('reason')?.value;
-
-//             // if (!(scheduleDays || dateFrom || timeFrom))
-//             //     throw new ScheduleError('Please select a schedule, date, or time.');
-//             // if (scheduleDays && dateFrom)
-//             //     throw new ScheduleError('Please select either a schedule or a date (not both).');
-//             if (dateTo && !dateFrom)
-//                 throw new ScheduleError('Please select a start date.');
-//             if (timeTo && !timeFrom)
-//                 throw new ScheduleError('Please select a start time.');
-
-//             //Default times to 0:00 if empty
-//             if(!timeFrom)
-//                 timeFrom = '0:00';
-//             if(!timeTo)
-//                 timeTo = '0:00';
-
-//             //Create a start and end dayjs obj
-//             const startUnavail = dayjs(`${dateFrom} ${timeFrom}`).format('MM-DD hh:mm A');
-//             const endUnavail = dayjs(`${dateTo} ${timeTo}`).format('MM-DD hh:mm A');
-
-//             if(!dayjs(startUnavail).isValid&&!dayjs(endUnavail).isValid)
-//                 throw new ScheduleError('Enter dates and times in proper formats');
-
-//             const unavail = createUnavailability(startUnavail, endUnavail, userId, userTag, reason);
-
-//             let reply = [
-//                 '```',
-//                 JSON.stringify(unavail, undefined, 2),
-//                 '```',
-//             ].join('\n');
-
-//             await interaction.editReply({
-//                 content: reply
-//             });
-//         }
-
-//         catch (error) {
-//             if (error.name === 'ScheduleError') {
-//                 await interaction.editReply({
-//                     ephemeral: true,
-//                     content: `*${error.message}*`
-//                 });
-//             }
-//             else {
-//                 console.log(error);
-//                 await interaction.editReply({
-//                     ephemeral: true,
-//                     content: `*Issue running command*`
-//                 });
-//             }
-//         }
-//     }
-// };
 
 //Callbacks
 //set-unavailability
@@ -252,10 +136,6 @@ const setUnavail = async (interaction) => {
         let timeTo = interaction.options.get('time-to')?.value;
         const reason = interaction.options.get('reason')?.value;
 
-        // if (!(scheduleDays || dateFrom || timeFrom))
-        //     throw new ScheduleError('Please select a schedule, date, or time.');
-        // if (scheduleDays && dateFrom)
-        //     throw new ScheduleError('Please select either a schedule or a date (not both).');
         if (dateTo && !dateFrom)
             throw new ScheduleError('Please select a start date.');
         if (timeTo && !timeFrom)
@@ -307,3 +187,7 @@ const setUnavail = async (interaction) => {
         }
     }
 };
+
+const setAvail = async (interaction) => {
+
+}
