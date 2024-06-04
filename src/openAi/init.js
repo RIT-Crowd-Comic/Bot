c5adbd50534718c1922977ca755cecdbd8b1793c
William Tallarico <wct9667@rit.edu>
2024-06-04 13:14:56 -0400

    openAi test

---
+++ src/openAi/init.js
@@ -0,0 +1,12 @@
+const {OpenAI} = require('openai');
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
+
+const openAiClient = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY
+})
+
+
+module.exports = {
+    openAiClient
+}
