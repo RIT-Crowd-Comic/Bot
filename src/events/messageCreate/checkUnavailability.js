259b206ddf11d3f702dd727fa4b13c71eccf5801
William Tallarico <wct9667@rit.edu>
2024-06-05 10:36:15 -0400

    lint fixes

--- src/events/messageCreate/checkUnavailability.js
+++ src/events/messageCreate/checkUnavailability.js
@@ -1,25 +1,103 @@
 const { getAvailabilityChannel } = require('../../utils/availability');
-const {openAiClient} = require('../../openAi/init');
+const { openAiClient } = require('../../openAi/init');
 
-module.exports = async (client, message) => {
-    try {
+const rememberUnavailability = (message, from, to, reason = 'a mystery event') =>
+{
+    message.reply({ content: `${message.author.globalName} is unavailable from ${to} to ${from} for ${reason}` });
+};
+
+const rememberAvailability = (message, from, to) =>
+{
+    message.reply({ content: `${message.author.globalName} is available from ${to} to ${from}` });
+};
+
+const unableToParse = (message) =>
+{
+    message.reply({ content: `${message.author.globalName} did not give data that could be parsed` });
+};
+
+const tools = [
+    {
+        'type':     'function',
+        'function': {
+            'name':        'rememberUnavailability',
+            'description': 'Determines if the user is busy between certain times ',
+            'parameters':  {
+                'type':       'object',
+                'properties': {
+                    'from': {
+                        'type':        'string',
+                        'description': 'The start date and time that the user is unavailable. In UTC.',
+                    },
+                    'to': {
+                        'type':        'string',
+                        'description': 'The end date and time that the user is unavailable. In UTC.',
+                    },
+                    'reason': {
+                        'type':        'string',
+                        'description': 'The reason why the user unavailable at those times.',
+                    }
+                },
+                'required': ['from', 'to'],
+            },
+        },
+    },
+    {
+        'type':     'function',
+        'function': {
+            'name':        'rememberAvailability',
+            'description': 'Determines if the user is available between certain times',
+            'parameters':  {
+                'type':       'object',
+                'properties': {
+                    'from': {
+                        'type':        'string',
+                        'description': 'The start date and time that the user is available. In UTC.',
+                    },
+                    'to': {
+                        'type':        'string',
+                        'description': 'The end date and time that the user is available. In UTC.',
+                    },
+                },
+                'required': ['from', 'to'],
+            },
+        },
+    },
+    {
+        'type':     'function',
+        'function': {
+            'name':        'unableToParse',
+            'description': 'If the user inputs data that cannot be interpreted',
+            'parameters':  {},
+        },
+    },
+];
+
+module.exports = async (client, message) =>
+{
+    try
+    {
 
         const availabilityChannel = await getAvailabilityChannel();
-        //only send a message if it's not from a bot and it's from the available channel
-        if (message.author.bot || !availabilityChannel || message.channelId !== availabilityChannel.id) {
+
+        // only send a message if it's not from a bot and it's from the available channel
+        if (message.author.bot || !availabilityChannel || message.channelId !== availabilityChannel.id)
+        {
             return;
         }
-    
+
         const response = await openAiClient.chat.completions.create({
-            model: "gpt-3.5-turbo",
-            messages: [{
-                "role": "user",
-                "content": message.content
-            }],
-            tools: tools,
-            tool_choice: "auto",
+            model:    'gpt-3.5-turbo',
+            messages: [
+                {
+                    'role':    'user',
+                    'content': message.content
+                }
+            ],
+            tools:       tools,
+            tool_choice: 'auto',
         }).catch((error) => console.log('OpenAI Error ' + error));
-        
+
         const output = response.choices[0].message;
         console.log(output);
 
@@ -27,99 +105,22 @@ module.exports = async (client, message) => {
         console.log(calledFunction.name);
 
         const action = {
-            'rememberUnavailability'  : () => rememberUnavailability(message, JSON.parse(calledFunction.arguments)["from"],JSON.parse(calledFunction.arguments)["to"],JSON.parse(calledFunction.arguments)["reason"]),
-            'rememberAvailability'    : () => rememberAvailability(message, JSON.parse(calledFunction.arguments)["from"],JSON.parse(calledFunction.arguments)["to"]),
-            'unableToParse'           : () => unableToParse(message),
+            'rememberUnavailability': () => rememberUnavailability(message, JSON.parse(calledFunction.arguments)['from'], JSON.parse(calledFunction.arguments)['to'], JSON.parse(calledFunction.arguments)['reason']),
+            'rememberAvailability':   () => rememberAvailability(message, JSON.parse(calledFunction.arguments)['from'], JSON.parse(calledFunction.arguments)['to']),
+            'unableToParse':          () => unableToParse(message),
         };
 
 
-        action[calledFunction.name](); 
+        action[calledFunction.name]();
 
-    } catch (error) {
-        console.log(error)
     }
-}
-
-
-const messagesTest = [{"role": "user", "content": "I am a good person"}];
-
-const tools = [
+    catch (error)
     {
-      "type": "function",
-      "function": {
-        "name": "rememberUnavailability",
-        "description": "Determines if the user is busy between certain times ",
-        "parameters": {
-          "type": "object",
-          "properties": {
-            "from": {
-              "type": "string",
-              "description": "The start date and time that the user is unavailable. In UTC.",
-            },
-            "to":{
-                "type" : "string",
-                "description": "The end date and time that the user is unavailable. In UTC.",
-            },
-            "reason":{
-                "type" : "string",
-                "description": "The reason why the user unavailable at those times.",
-            }
-          },
-          "required": ["from", "to"],
-        },
-      },
-    },
-    {
-        "type": "function",
-        "function": {
-            "name": "rememberAvailability",
-            "description": "Determines if the user is available between certain times",
-            "parameters": {
-                "type": "object",
-                "properties": {
-                  "from": {
-                    "type": "string",
-                    "description": "The start date and time that the user is available. In UTC.",
-                  },
-                  "to":{
-                      "type" : "string",
-                      "description": "The end date and time that the user is available. In UTC.",
-                  },
-                },
-                "required": ["from", "to"],
-            },
-        },
-    },
-    {
-        "type": "function",
-        "function": {
-            "name": "unableToParse",
-            "description": "If the user inputs data that cannot be interpreted",
-            "parameters": {},
-        },
-    },
-];
+        console.log(error);
+    }
+};
 
-
-const rememberUnavailability = (message, from, to, reason = 'a mystery event') =>{
-    message.reply({
-        content: `${message.author.globalName} is unavailable from ${to} to ${from} for ${reason}`
-    })
-}
-
-const rememberAvailability = (message, from, to) =>{
-    message.reply({
-        content: `${message.author.globalName} is available from ${to} to ${from}`
-    })
-}
-
-const unableToParse = (message) =>{
-    message.reply({
-        content: `${message.author.globalName} did not give data that could be parsed`
-    })
-}
-
-/*{
+/* {
   from: <etc>,
   to: <etc>,
   reason: 'fourth'
--- src/openAi/init.js
+++ src/openAi/init.js
@@ -1,12 +1,8 @@
-const {OpenAI} = require('openai');
+const { OpenAI } = require('openai');
 const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
 
-const openAiClient = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY
-})
+const openAiClient = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
 
-module.exports = {
-    openAiClient
-}
+module.exports = { openAiClient };
