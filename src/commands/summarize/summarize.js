8dd1dfc5e0a9c4379c42153c5e23802b83437125
William Tallarico <wct9667@rit.edu>
2024-06-10 15:54:39 -0400

    summarize start

--- src/commands/summarize/summarize.js
+++ src/commands/summarize/summarize.js
@@ -1,24 +1,46 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { help, helpRemember, helpAvailability } = require('../../utils/helpCommands');
+const { openAiClient } = require('../../openAi/init');
+const { getRememberedMessages } = require('../../utils/rememberMessages');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('summarize')
         .setDescription('Summarizes messages that were remembered.')
-        .addNumberOption(option =>{
+        .addNumberOption(option =>
             option.setName('number')
-            .setDescription('Number of messages to summarize')
-        }),
+                .setDescription('Number of messages to summarize')
+                .setRequired(false)
+        ),
+
 
     async execute(client, interaction) {
         try {
+
+            //grab the option 
+            const num = interaction.options.getNumber('number');
+            
             //defer reply
             await interaction.deferReply();
 
+            const numStr = num !== null ? `the most recent ${num.toString()}` : 'all'
+
             //openai stuff
+            const prompt = `Summarize the 'content' of the following array of messages, listing the messages in the order they appear: ${JSON.stringify(getRememberedMessages(), null, 2)}`
+
+            const response = await openAiClient.chat.completions.create({
+                model:    'gpt-3.5-turbo',
+                messages: [
+                    {
+                        'role':    'user',
+                        'content': prompt,
+                    }
+                ]
+            }).catch((error) => console.log('OpenAI Error ' + error));
+
+            await interaction.editReply(response.choices[0].message.content);
         }
         catch (error) {
-            await interaction.reply({
+            await interaction.editReply({
                 content:   `Something went wrong. ${error}`,
                 ephemeral: false,
             });
