e120713648e26153dc38ed11abe03545ead68642
William Tallarico <wct9667@rit.edu>
2024-06-12 11:33:04 -0400

    updated prompt

--- src/commands/summarize/summarize.js
+++ src/commands/summarize/summarize.js
@@ -7,11 +7,6 @@ module.exports = {
         .setName('summarize')
         .setDescription('Summarizes messages that were remembered.')
         .addNumberOption(option =>
-            option.setName('number')
-                .setDescription('Number of messages to summarize')
-                .setRequired(false)
-        )
-        .addNumberOption(option =>
             option.setName('number-words')
                 .setDescription('Length in words of the summary')
                 .setRequired(false)
@@ -20,20 +15,15 @@ module.exports = {
 
     async execute(client, interaction) {
         try {
-
-            //grab the option 
-            const numToSummarize = interaction.options.getNumber('number');
-
             const numWords = interaction.options.getNumber('number-words');
             
             //defer reply
             await interaction.deferReply();
 
-            const numToSummarizeStr = numToSummarize !== null ? `the most recent ${numToSummarize.toString()}` : 'all'
             const numWordsStr = numWords !== null ? `The summary has to be ${numWords.toString()} words long.` : 'The summary can be any length.'
 
             //openai stuff
-            const prompt = `Summarize the 'content' of the following array of messages, listing the messages in the order they appear. ${numWordsStr}: ${JSON.stringify(getRememberedMessages(), null, 2)}`
+            const prompt = `Summarize the 'content' of the following array of messages, listing the messages in the order they appear.  ${numWordsStr}: ${JSON.stringify(getRememberedMessages(), null, 2)}`
 
             const response = await openAiClient.chat.completions.create({
                 model:    'gpt-3.5-turbo',
