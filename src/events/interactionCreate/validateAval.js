158cde0114d6ba041781c4d28958c0a933d12e49
William Tallarico <wct9667@rit.edu>
2024-06-13 15:51:01 -0400

    hotfix

--- src/events/interactionCreate/validateAval.js
+++ src/events/interactionCreate/validateAval.js
@@ -4,7 +4,7 @@ const path = './src/savedAvailability.json';
 module.exports = async (client, interaction) => {
     if (!interaction.isButton()) return;
 
-    const [action, globalName, id, start, end, monday, tuesday, wednesday, thursday, friday] = interaction.customId.split('_');
+    const [action, id, start, end, monday, tuesday, wednesday, thursday, friday] = interaction.customId.split('_');
     if (action === 'v-a-y') {
         interaction.reply({ content: 'Data Saved', ephemeral: true });
 
@@ -12,7 +12,9 @@ module.exports = async (client, interaction) => {
             .map((value, index) => ((value === 't') ? daysOfWeek[index] : null)) // Map days to corresponding day names or null
             .filter(day => day !== null);
 
-        setAvail(id, globalName, start, end, mappedDays, path);
+        const user = await client.users.fetch(id);
+
+        setAvail(id, user.globalName, start, end, mappedDays, path);
 
         // Remove the button by editing the message
         await interaction.message.edit({ components: [] });
