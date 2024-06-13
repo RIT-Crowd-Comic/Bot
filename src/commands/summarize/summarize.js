14a823b181482f4e69efba724f83315506856106
William Tallarico <wct9667@rit.edu>
2024-06-13 12:59:30 -0400

    summarize refinements

--- src/commands/summarize/summarize.js
+++ src/commands/summarize/summarize.js
@@ -1,6 +1,8 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { openAiClient } = require('../../openAi/init');
 const { getRememberedMessages } = require('../../utils/rememberMessages');
+const { stripIndent } = require('common-tags');
+const {splitMessageToFitTokenLimit} = require('../../openAi/splitToken')
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -13,6 +15,7 @@ module.exports = {
         ),
 
 
+
     async execute(client, interaction) {
         try {
             const numWords = interaction.options.getNumber('number-words');
@@ -22,8 +25,19 @@ module.exports = {
 
             const numWordsStr = numWords !== null ? `The summary has to be ${numWords.toString()} words long.` : 'The summary can be any length.'
 
+            //get messages and remove piece of the message object we dont want to reduce tokens
+
+            const messages = getRememberedMessages().map(message =>{
+                return {
+                    globalName: message.author.globalName,
+                    content: message.content,
+                };
+            }); 
+  
             //openai stuff
-            const prompt = `Summarize the 'content' of the following array of messages, listing the messages in the order they appear.  ${numWordsStr}: ${JSON.stringify(getRememberedMessages(), null, 2)}`
+            const prompt = stripIndent`Summarize the 'content' of the following array of messages, 
+            listing the messages in the order they appear. ${numWordsStr}: 
+            ${splitMessageToFitTokenLimit(JSON.stringify(messages, null, 2))[0]}`
 
             const response = await openAiClient.chat.completions.create({
                 model:    'gpt-3.5-turbo',
@@ -33,13 +47,13 @@ module.exports = {
                         'content': prompt,
                     }
                 ]
-            }).catch((error) => console.log('OpenAI Error ' + error));
+            });
 
             await interaction.editReply(response.choices[0].message.content);
         }
         catch (error) {
             await interaction.editReply({
-                content:   `Something went wrong. ${error}`,
+                content:   `Something went wrong.  Ensure that not too many messages are being summarized. ${error}`,
                 ephemeral: false,
             });
         }
---
+++ src/openAi/splitToken.js
@@ -0,0 +1,38 @@
+const { encode, decode } = require('gpt-3-encoder');
+
+const TOKEN_LIMIT = 4096;
+
+/**
+ * 
+ * @param {string} prompt 
+ * @param {number} tokenLimit 
+ * @returns 
+ */
+const  splitMessageToFitTokenLimit = (prompt, tokenLimit = TOKEN_LIMIT) => {
+    //encode
+    const tokens = encode(prompt);
+    //grab num tokens
+    const tokenCount = tokens.length;
+
+    //if under return
+    if (tokenCount <= tokenLimit) {
+        return [prompt];
+    }
+
+    //split into segments and return array
+    const segments = [];
+    let start = 0;
+    while (start < tokenCount) {
+        const end = Math.min(start + tokenLimit, tokenCount);
+        const segmentTokens = tokens.slice(start, end);
+        const segment = decode(segmentTokens);
+        segments.push(segment);
+        start = end;
+    }
+
+    return segments;
+}
+
+module.exports = {
+    splitMessageToFitTokenLimit
+}
