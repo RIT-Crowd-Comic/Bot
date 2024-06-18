6bb777f74b020f53771747b5992ab7f6f124b4d7
apowers131 <arthurpow131@gmail.com>
2024-06-17 17:17:28 -0700

    removed test database queries

--- src/index.js
+++ src/index.js
@@ -22,28 +22,4 @@ eventHandler(client);
 db.authenticate()
     .then(() => void console.log('Successfully connected to database!'));
 
-// test a bunch of database stuff
-// remove this entire section when publishing
-(async () => {
-    console.log('database tests starting');
-
-    // ESPECIALLY REMOVE .sync()
-    // THESE LINES BREAK DATABASES IN PRODUCTION
-    // await db.Models.User.sync({ force: true });
-    // await db.Models.CheckInResponse.sync({ force: true });
-    // await db.Models.UnavailableSchedule.sync({ force: true });
-    // await db.Models.AvailableSchedule.sync({ force: true });
-    // await db.Models.CheckInSchedule.sync({ force: true });
-    // await db.Models.Config.sync();
-    // await db.Models.Message.sync({ force: true });
-    // await db.Models.UnavailableStart.sync({ force: true });
-    // await db.Models.UnavailableStop.sync({ force: true });
-    // await db.Models.CheckInReminder.sync({ force: true });
-
-})()
-    .then(() => {
-        console.log('fin');
-    });
-
-
 client.login(process.env.DISCORD_TOKEN);
